25fe1f9ba50cc6dd898615b45de9dd964a1f3f9e
federicobaldini <federico-baldini@hotmail.com>
2021-11-16 21:23:24 +0100

    added new Header component

---
+++ src/UI/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<style>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    background-color: #cf0056;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.25);
+  }
+
+  h1 {
+    color: white;
+    font-style: 'Roboto Slab', serif;
+    margin: 0;
+  }
+</style>
+
+<header>
+  <h1>MeetUp</h1>
+</header>
