55214da6d80a7133847584009fc0b8a8ce3ab5ef
federicobaldini <federico-baldini@hotmail.com>
2021-12-08 10:02:22 +0100

    added new MeetupFilter component

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,11 +1,23 @@
 <script>
   import MeetupItem from "./MeetupItem.svelte";
+  import MeetupFilter from "./MeetupFilter.svelte";
 
   export let meetups;
+
+  let favoritesOnly = false;
+
+  $: filteredMeetups = favoritesOnly ? meetups.filter((m) => m.isFavorite) : meetups;
+
+  const setFilter = (event) => {
+    favoritesOnly = event.detail === 1;
+  };
 </script>
 
+<section id="meetup-controls">
+  <MeetupFilter on:select={setFilter} />
+</section>
 <section id="meetups">
-  {#each meetups as meetup}
+  {#each filteredMeetups as meetup}
     <MeetupItem
       id={meetup.id}
       title={meetup.title}
@@ -21,15 +33,19 @@
 </section>
 
 <style>
-  section {
+  #meetups {
     width: 100%;
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 1rem;
   }
 
+  #meetup-controls {
+    margin: 1rem;
+  }
+
   @media (min-width: 768px) {
-    section {
+    #meetups {
       grid-template-columns: repeat(2, 1fr);
     }
   }
